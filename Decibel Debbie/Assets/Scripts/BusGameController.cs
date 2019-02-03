0b79bc2c80817cee6318673dbddf41b0a3ba20ae
alex.mike.mark <alex.mike.mark@gmail.com>
2019-02-03 08:47:08 -0600

    add timer.

---
+++ Decibel Debbie/Assets/Scripts/BusGameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BusGameController : MonoBehaviour
+{
+    public float duration; // How long this level will last for.
+    public Text timeDisplay;
+    
+    private float remainingTime;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        duration = duration - Time.deltaTime;
+        timeDisplay.text = ""+(Mathf.Ceil(duration));
+        if(duration<0){
+            gameOver(true);
+        }
+    }
+
+    void gameOver(bool victory){
+        if ( victory ){
+            // GO TO SCENE
+        } else {
+            // GO TO GAME OVER
+        }
+    }
+}
