b34eb9941fd0c29878c049f46eb5fa815b642c0c
Jon <ingramj@myumanitoba.ca>
2019-02-03 19:32:52 -0600

    Added cheers

---
+++ Decibel Debbie/Assets/Scripts/MusicScene/CrowdController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdController : MonoBehaviour
+{
+    private AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        StartCoroutine(ShowTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator ShowTime()
+    {
+        yield return new WaitForSeconds(3);
+        sound.Play();
+    }
+}
